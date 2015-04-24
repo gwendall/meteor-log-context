6d09689b42b723b1cf9b182d811feb2e03cad27d
Gwen <gwendall.esnault@gmail.com>
2015-04-24 11:53:47 +0200

    first commit

---
+++ lib.js
@@ -0,0 +1,19 @@
+// Returns the current and parents data contexts
+Template.logContext.events({
+  "click [data-log-context]": function(e, tpl) {
+    var element = $(e.currentTarget);
+    var limit = Number(element.attr("data-log-context")) || 10;
+    console.log("Blaze context.", {
+      data: Blaze.getData(element.get(0)),
+      tpls: _.map(_.range(0, limit), function(i) {
+        return tpl.parent(i);
+      })
+    });
+  }
+});
+
+Template.logContext.helpers({
+  "nbParents": function() {
+    return this.nbParents || 10;
+  }
+});
---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: "gwendall:log-this",
+  summary: "Log Blaze data context with a click",
+  version: "0.1.0"
+});
+
+Package.on_use(function (api, where) {
+
+  api.use([
+    "templating@1.0.11",
+    "aldeed:template-extension@3.4.3"
+  ], "client");
+
+  api.add_files([
+    "lib.html",
+    "lib.js",
+  ], "client");
+
+});
